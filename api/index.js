a454960a18cd837795af6be60b7d1d7185b181cc
Zaheer_Ahmad <zaheerahmadlangrial92@gmail.com>
2024-03-18 22:45:44 +0500

    Connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Done hugyaa.....')
+}).catch((error) => {
+    console.log(error)
+})
 
 const app = express();
+
+// 
 
 app.listen(3000 , () => {
     console.log('Server listening on port 3000')
