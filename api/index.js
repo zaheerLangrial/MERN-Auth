e90f8577d8b892b8a50e64b8b5103ac13a5f6fce
Zaheer_Ahmad <zaheerahmadlangrial92@gmail.com>
2024-03-22 00:02:46 +0500

    Add a Middlware and create a function to handle errors

--- api/index.js
+++ api/index.js
@@ -30,3 +30,14 @@ app.get('/' , (req , res) => {
 
 app.use('/api/user' , userRoutes)
 app.use('/api/auth' , authRoutes)
+
+
+app.use((err , req , res , next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internet server error';
+    return res.status(statusCode).json({
+        success : false,
+        message,
+        statusCode,
+    })
+})
