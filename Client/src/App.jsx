359b38925612cc89f1f4cbee64e92c556df16452
Zaheer_Ahmad <zaheerahmadlangrial92@gmail.com>
2024-03-29 16:04:19 +0500

    Make header update and profile private

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Nav from './components/Nav'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
 
@@ -14,7 +15,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
       </Routes>
--- Client/src/components/Nav.jsx
+++ Client/src/components/Nav.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 function Nav() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <div className='bg-gray-200 w-full py-5'>
         <div className=' max-w-6xl mx-auto flex justify-between items-center'>
@@ -14,7 +16,16 @@ function Nav() {
             <ul className='flex gap-5'>
                 <Link to={'/'}>Home</Link>
                 <Link to={'/about'}>About</Link>
-                <Link to={'/sign-in'}>Sign in</Link>
+                <Link to={'/profile'}>
+                {
+                  currentUser ? (
+                    <img src={currentUser.profilePicture} alt="Profile pic" className='w-7 h-7 object-cover rounded-full' />
+                  ) : 
+                    <li>
+                      Sign in
+                    </li>
+                }
+                </Link>
             </ul>
         </div>
     </div>
